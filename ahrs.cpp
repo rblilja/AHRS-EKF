c6ef4618404d4be63025ceaa9b75b4f328f4d7c6
Robin <robin@Robins-MacBook-Pro-Work.local>
2020-12-08 14:25:04 +0100

    initial

---
+++ ahrs.cpp
@@ -0,0 +1,461 @@
+/*
+ *	Copyright 2020 Robin Lilja <robin.lilja@gmail.com>
+ *
+ *  Created on: Jun 26, 2015
+ *      Author: Robin Lilja
+ * 		License: MIT License (see LICENSE file)
+ */
+
+#include "ahrs.h"
+
+namespace AHRS {
+
+template <typename T>
+void AHRS<T>::propagate(const T &wx, const T &wy, const T &wz, const T &dt) {
+
+	// State vector is defined as x = [q0 q1 q2 q3 bwx bwy bwz]'
+
+	// Body rates are represented as quaternion time derivative 'qdot' by the following
+	// d(quaternion)/dt = 1/2 * [  0 -wx -wy -wz ][q0]
+	// 							[ wx   0  wz -wy ][q1]
+  	// 							[ wy -wz   0  wx ][q2]
+  	// 							[ wz  wy -wx   0 ][q3]
+	//
+	
+	// If the sampling rate is sufficiently high, the quaternion dynamics behave in a
+	// quasi-linear fashion since, with small timesteps, the integration steps propagate the quaternions
+	// only small deviations away from the unit sphere, making the error in linearization minimal.
+
+	// Remove bias from rate measurements
+	m_wx = wx - m_bwx;
+	m_wy = wy - m_bwy;
+	m_wz = wz - m_bwz;
+
+	// Calculate the linearized sate model 'F_k-1 = d(xdot)/dx' around the current estimated state 'xhat'
+	//
+	// Repeated arithmetics for the linearized state model
+	const T _wx2 = 0.5 * m_wx * dt;
+	const T _wy2 = 0.5 * m_wy * dt;
+	const T _wz2 = 0.5 * m_wz * dt;
+	//
+	const T _q02 = 0.5 * m_q0 * dt;
+	const T _q12 = 0.5 * m_q1 * dt;
+	const T _q22 = 0.5 * m_q2 * dt;
+	const T _q32 = 0.5 * m_q3 * dt;
+	//
+	// Discretization by (I + F*dt) is included in the 'F_k-1' matrix for convenience. Note that angular rates has been compensated for bias already.
+	const T F[7][7] =
+	{
+		{ 1.0, -_wx2, -_wy2, -_wz2, _q12, _q22, _q32 },
+		{ _wx2,	1.0, _wz2, -_wy2, -_q02, _q32, -_q22 },
+		{ _wy2,	-_wz2, 1.0, _wx2, -_q32, -_q02, _q12 },
+		{ _wz2,	_wy2, -_wx2, 1.0, _q22, -_q12, -_q02 },
+		{ 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0 },
+		{ 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0 },
+		{ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0 }
+	};
+
+	T FP[7];		// Intermediate array holding the i:th row of the 'F_k-1 * P_k-1|k-1' multiplication product
+	T P_k[7][7];	// The new state estimate covariance matrix 'P_k|k-1'
+
+	// Calculate the state estimate covariance 'P_k|k-1 = F_k-1 * P_k-1|k-1 * F'_k-1 + Q_k-1'
+	//
+	// Begin with the multiplications
+	for (uint8_t i=0; i<7; ++i) {
+
+		// Calculate i:th row of the 'F_k-1 * P_k-1|k-1' multiplication product
+		for (uint8_t j=0; j<7; ++j) {
+
+#ifdef AHRS_UNROLLED
+
+			// Exploit the fact that 'F_k-1' contains quite a few zeros, and some ones on the diagonal
+			switch (i) {
+				case 0: FP[j] =         m_P[0][j] + F[i][1]*m_P[1][j] + F[i][2]*m_P[2][j] + F[i][3]*m_P[3][j] + F[i][4]*m_P[4][j] + F[i][5]*m_P[5][j] + F[i][6]*m_P[6][j]; break;
+				case 1: FP[j] = F[i][0]*m_P[0][j] +         m_P[1][j] + F[i][2]*m_P[2][j] + F[i][3]*m_P[3][j] + F[i][4]*m_P[4][j] + F[i][5]*m_P[5][j] + F[i][6]*m_P[6][j]; break;
+				case 2: FP[j] = F[i][0]*m_P[0][j] + F[i][1]*m_P[1][j] +         m_P[2][j] + F[i][3]*m_P[3][j] + F[i][4]*m_P[4][j] + F[i][5]*m_P[5][j] + F[i][6]*m_P[6][j]; break;
+				case 3: FP[j] = F[i][0]*m_P[0][j] + F[i][1]*m_P[1][j] + F[i][2]*m_P[2][j] +         m_P[3][j] + F[i][4]*m_P[4][j] + F[i][5]*m_P[5][j] + F[i][6]*m_P[6][j]; break;
+				case 4: FP[j] =                                                                         		m_P[4][j];                                     			   break;
+				case 5: FP[j] =                                                                                          		  	m_P[5][j];                   		   break;
+				case 6: FP[j] =                                                                                                             			m_P[6][j];
+			}
+#elif
+			// Without exploitation of zeros the following unrolled calculation shall be used
+			FP[j] = F[i][0]*m_P[0][j] + F[i][1]*m_P[1][j] + F[i][2]*m_P[2][j] + F[i][3]*m_P[3][j] + F[i][4]*m_P[4][j] + F[i][5]*m_P[5][j] + F[i][6]*m_P[6][j];
+#endif
+		}
+
+		// Calculate the i:th row of 'P_k|k-1' by multiplying the intermediate i:th row with the transpose of 'F_k-1'
+		for (uint8_t j=0; j<7; ++j) {
+
+#ifdef AHRS_UNROLLED
+
+			// As for 'F_k-1' we exploit the fact that its transpose contains zeros and ones as well
+			// Note that the transpose is not necessary to calculate explicitly, instead of multiplying the i:th row with each column of 'F'_k-1' (the transpose) we
+			// simply multiply with the rows of 'F_k-1' instead
+			switch (j) {
+				case 0: P_k[i][j] = FP[0]         + FP[1]*F[j][1] + FP[2]*F[j][2] + FP[3]*F[j][3] + FP[4]*F[j][4] + FP[5]*F[j][5] + FP[6]*F[j][6]; break;
+				case 1: P_k[i][j] = FP[0]*F[j][0] + FP[1]         + FP[2]*F[j][2] + FP[3]*F[j][3] + FP[4]*F[j][4] + FP[5]*F[j][5] + FP[6]*F[j][6]; break;
+				case 2: P_k[i][j] = FP[0]*F[j][0] + FP[1]*F[j][1] + FP[2]         + FP[3]*F[j][3] + FP[4]*F[j][4] + FP[5]*F[j][5] + FP[6]*F[j][6]; break;
+				case 3: P_k[i][j] = FP[0]*F[j][0] + FP[1]*F[j][1] + FP[2]*F[j][2] + FP[3]         + FP[4]*F[j][4] + FP[5]*F[j][5] + FP[6]*F[j][6]; break;
+				case 4: P_k[i][j] =                                                                 FP[4];		                                   break;
+				case 5: P_k[i][j] =                                                                                 FP[5];		                   break;
+				case 6: P_k[i][j] =                                                                                                 FP[6];
+			}
+#elif
+			// Without exploitation of zeros the following unrolled calculation shall be used
+			P_k[i][j] = FP[0]*F[j][0] + FP[1]*F[j][1] + FP[2]*F[j][2] + FP[3]*F[j][3] + FP[4]*F[j][4] + FP[5]*F[j][5] + FP[6]*F[j][6];
+#endif
+		}
+	}
+	//
+	// Finalize 'P_k|k-1' by adding process noise, for the sake of computational simplicity only a diagonal noise matrix 'Q_k-1' is considered
+	//
+	// Some repeated arithmetics
+	T _Qa = 0.5 * dt * m_Qa;
+	T _Qb = 0.5 * dt * m_Qb;
+	//
+	P_k[0][0] += _Qa;
+	P_k[1][1] += _Qa;
+	P_k[2][2] += _Qa;
+	P_k[3][3] += _Qa;
+	P_k[4][4] += _Qb;
+	P_k[5][5] += _Qb;
+	P_k[6][6] += _Qb;
+
+	// Take a copy of the state estimate covariance matrix
+	memcpy(m_P, P_k, sizeof(T)*49);
+
+	// Calculate the state derivative 'xdot', effectively being the rate quaternion 'qdot'
+	//
+	T qdot0, qdot1, qdot2, qdot3;
+	//
+	qdot0 = 0.5 * (-m_q1 * m_wx - m_q2 * m_wy - m_q3 * m_wz );
+	qdot1 = 0.5 * ( m_q0 * m_wx + m_q2 * m_wz - m_q3 * m_wy );
+	qdot2 = 0.5 * ( m_q0 * m_wy - m_q1 * m_wz + m_q3 * m_wx );
+	qdot3 = 0.5 * ( m_q0 * m_wz + m_q1 * m_wy - m_q2 * m_wx );
+
+	// Propagate the 'xhat' state estimate 'dt' seconds from time instance 'k-1' to 'k'
+	//
+	// Euler forward integration
+	m_q0 += qdot0 * dt;
+	m_q1 += qdot1 * dt;
+	m_q2 += qdot2 * dt;
+	m_q3 += qdot3 * dt;
+	//
+	// Normalize quaternion
+	T reciprocal = 1.0 / std::sqrt(m_q0 * m_q0 + m_q1 * m_q1 + m_q2 * m_q2 + m_q3 * m_q3);
+	m_q0 *= reciprocal;
+	m_q1 *= reciprocal;
+	m_q2 *= reciprocal;
+	m_q3 *= reciprocal;
+}
+
+template <typename T>
+void AHRS<T>::accelerometer_update(T ax, T ay, T az) {
+
+	// Normalize accelerometer measurement
+	const T reciprocal = 1.0 / std::sqrt(ax * ax + ay * ay + az * az);
+	ax *= reciprocal;
+	ay *= reciprocal;
+	az *= reciprocal;
+
+	// Repeated arithmetics used the filter update
+	const T _2q0 = 2.0 * m_q0;
+	const T _2q1 = 2.0 * m_q1;
+	const T _2q2 = 2.0 * m_q2;
+	const T _2q3 = 2.0 * m_q3;
+	//
+	const T _q0q0 = m_q0 * m_q0;
+	const T _q1q1 = m_q1 * m_q1;
+	const T _q2q2 = m_q2 * m_q2;
+	const T _q3q3 = m_q3 * m_q3;
+	//
+	const T _2q1q0 = 2.0 * (m_q1 * m_q0);
+	//const T _2q1q2 = 2.0 * (m_q1 * m_q2);
+	const T _2q1q3 = 2.0 * (m_q1 * m_q3);
+	//
+	const T _2q2q0 = 2.0 * (m_q2 * m_q0);
+	//const T _2q3q0 = 2.0 * (m_q3 * m_q0);
+	//
+	const T _2q2q3 = 2.0 * (m_q2 * m_q3);
+
+	// 'H = d(xhat)/dx'
+	const T H[6][7] =
+	{
+		{ -_2q2,	_2q3,		-_2q0,		_2q1,		0.0,	0.0,	0.0 },
+		{ _2q1,		_2q0,		_2q3,		_2q2,		0.0,	0.0,	0.0 },
+		{ _2q0,		-_2q1,		-_2q2,		_2q3,		0.0,	0.0,	0.0 },
+		{ _2q0,		_2q1,		-_2q2,		-_2q3,		0.0,	0.0, 	0.0 },
+		{ -_2q3,	_2q2,		_2q1,		-_2q0,		0.0,	0.0, 	0.0 },
+		{ _2q2,		_2q3,		_2q0,		_2q1,		0.0,	0.0, 	0.0 }
+	};
+
+	// // Magnetometer measurement is present
+	// if ((sensors & MAGNETOMETER)  == MAGNETOMETER) {
+
+	// 	// Transform measured body frame magnetic vector into Earth (NED) frame, the z-axis is neglected
+	// 	// since the magnetic "heading" vector in the Earth (NED) frame is always in the xy-plane i.e. north and east directions
+	// 	//
+	// 	// Be = DCM' * Bb
+	// 	T Bex = (_q0q0 + _q1q1 - _q2q2 - _q3q3)*Bx + (_2q1q2 - _2q3q0)*By + (_2q1q3 + _2q2q0)*Bz;
+	// 	T Bey = (_2q1q2 + _2q3q0)*Bx + (_q0q0 - _q1q1 + _q2q2 - _q3q3)*By + (_2q2q3 - _2q1q0)*Bz;
+
+	// 	// Normalize the magnetic vector, it will now be a unit vector in the horizontal plane of the NED frame
+	// 	reciprocal = 1.0 / sqrt(Bex * Bex + Bey * Bey);
+	// 	Bex *= reciprocal;
+	// 	Bey *= reciprocal;
+
+	// 	// Transform the vector back to the body frame
+	// 	//
+	// 	// Bb = DCM * Be
+	// 	Bx = (_q0q0 + _q1q1 - _q2q2 - _q3q3)*Bex + (_2q1q2 + _2q3q0)*Bey;
+	// 	By = (_2q1q2 - _2q3q0)*Bex + (_q0q0 - _q1q1 + _q2q2 - _q3q3)*Bey;
+	// 	Bz = (_2q1q3 + _2q2q0)*Bex + (_2q2q3 - _2q1q0)*Bey;
+	// }
+
+	// Innovation vector 'y' being the difference between actual observation vector 'z' and
+	// recreation of observation vector 'zhat' from the current state estimate 'xhat'
+	//
+	// The following rotation matrix (DCM) transform a vector in the Earth (NED) coordinate frame into the body frame of reference
+	//
+	// [ (q0*q0 + q1*q1 - q2*q2 - q3*q3)	2*(q1*q2 + q3*q0)					2*(q1*q3 - q2*q0)		    	]
+	// [ 2*(q1*q2 - q3*q0)    				(q0*q0 - q1*q1 + q2*q2 - q3*q3) 	2*(q2*q3 + q1*q0)				]
+	// [ 2*(q1*q3 + q2*q0)    				2*(q2*q3 - q1*q0)					(q0*q0 - q1*q1 - q2*q2 + q3*q3)	]
+	//
+	// In the Earth (NED) frame two reference vectors exist, one being the acceleration vector [0 0 1]' pointing at Earth's center
+	// and the second one being the magnetic field vector [1 0 0]' pointing at Earth's magnetic north.
+	//
+	// Our task here is to recreate those vectors from the current estimated state and express them in the body frame of reference.
+	//
+	// When the rotation matrix is used for transforming the NED gravitation and magnetic vectors into the body frame, the former only receives
+	// contribution from the third column, and the latter only from the first column.
+	//
+	const T y[3] =
+	{
+		ax - (_2q1q3 - _2q2q0),
+		ay - (_2q2q3 + _2q1q0),
+		az - (_q0q0 - _q1q1 - _q2q2 + _q3q3)
+		// Bx - (_q0q0 + _q1q1 - _q2q2 - _q3q3),
+		// By - (_2q1q2 - _2q3q0),
+		// Bz - (_2q1q3 + _2q2q0)
+	};
+
+	T S;		// The innovation covariance 'S_k'
+	T Sinv;		// The inverse of the innovation covariance 'S_k'
+	T K[7];		// The Kalman gain 'K_k'
+	T HP[7];	// Used for calculating both 'S_k' and 'K_k'
+
+	// Sequential update, see references: 
+    // 1) KALMAN FILTERING Theory and Practice Using MATLAB, 4.2.2, p. 141, 3:rd
+    // 2) http://www.anuncommonlab.com/articles/how-kalman-filters-work/part2.html#speed
+	for (uint8_t m=0; m<3; ++m) {
+
+		// Calculate the m:th row of the 'H_k * P_k|k-1' multiplication product
+		//
+		// Iterate the columns of 'P_k|k-1'
+		for (uint8_t i=0; i<7; ++i) {
+
+			HP[i] = H[m][0]*m_P[0][i] + H[m][1]*m_P[1][i] + H[m][2]*m_P[2][i] + H[m][3]*m_P[3][i];
+		}
+
+		// Calculate the innovation covariance 'S_k = H_k * P_k|k-1 * H'_k + R_k'
+		S = HP[0]*H[m][0] + HP[1]*H[m][1] + HP[2]*H[m][2] + HP[3]*H[m][3] + m_Ra;
+		//
+		// Multiplying is faster than dividing (so divide once)
+		Sinv = 1.0 / (S + AHRS_SAFE_EPSILON);
+
+		// Calculate the Kalman gain 'K_k = (H_k * P_k|k-1) / S
+		for (uint8_t i=0; i<7; ++i) {
+
+			K[i] = HP[i]*Sinv;
+		}
+
+		// Update the state estimate covariance 'P_k|k = P_k|k-1 - K_k * H_k * P_k|k-1'
+		for (uint8_t i=0; i<7; ++i) {
+
+			// Exploit mirrored values over the diagonal
+			for (uint8_t j=i; j<7; ++j) {
+
+				m_P[i][j] = m_P[j][i] = m_P[i][j] - K[i]*HP[j];
+			}
+		}
+
+		// Update the state estimate 'xhat_k|k = xhat_k|k-1 + K_k * y_k'
+		m_q0 += K[0] * y[m];
+		m_q1 += K[1] * y[m];
+		m_q2 += K[2] * y[m];
+		m_q3 += K[3] * y[m];
+		m_bwx += K[4] * y[m];
+		m_bwy += K[5] * y[m];
+		m_bwz += K[6] * y[m];
+	}
+}
+
+template <typename T>
+void AHRS<T>::magnetometer_update(T Bx, T By, T Bz) {
+
+	// Repeated arithmetics used the filter update
+	const T _2q0 = 2.0 * m_q0;
+	const T _2q1 = 2.0 * m_q1;
+	const T _2q2 = 2.0 * m_q2;
+	const T _2q3 = 2.0 * m_q3;
+	//
+	const T _q0q0 = m_q0 * m_q0;
+	const T _q1q1 = m_q1 * m_q1;
+	const T _q2q2 = m_q2 * m_q2;
+	const T _q3q3 = m_q3 * m_q3;
+	//
+	const T _2q1q0 = 2.0 * (m_q1 * m_q0);
+	const T _2q1q2 = 2.0 * (m_q1 * m_q2);
+	const T _2q1q3 = 2.0 * (m_q1 * m_q3);
+	//
+	const T _2q2q0 = 2.0 * (m_q2 * m_q0);
+	const T _2q3q0 = 2.0 * (m_q3 * m_q0);
+	//
+	const T _2q2q3 = 2.0 * (m_q2 * m_q3);
+
+	// 'H = d(xhat)/dx'
+	const T H[6][7] =
+	{
+		{ -_2q2,	_2q3,		-_2q0,		_2q1,		0.0,	0.0,	0.0 },
+		{ _2q1,		_2q0,		_2q3,		_2q2,		0.0,	0.0,	0.0 },
+		{ _2q0,		-_2q1,		-_2q2,		_2q3,		0.0,	0.0,	0.0 },
+		{ _2q0,		_2q1,		-_2q2,		-_2q3,		0.0,	0.0, 	0.0 },
+		{ -_2q3,	_2q2,		_2q1,		-_2q0,		0.0,	0.0, 	0.0 },
+		{ _2q2,		_2q3,		_2q0,		_2q1,		0.0,	0.0, 	0.0 }
+	};
+
+	// Transform measured body frame magnetic vector into Earth (NED) frame, the z-axis is neglected
+	// since the magnetic heading vector in the Earth (NED) frame is always in the xy-plane i.e. north and east directions
+	//
+	// Be = DCM' * Bb
+	T Bex = (_q0q0 + _q1q1 - _q2q2 - _q3q3) * Bx + (_2q1q2 - _2q3q0) 			   * By + (_2q1q3 + _2q2q0) * Bz;
+	T Bey = (_2q1q2 + _2q3q0)				* Bx + (_q0q0 - _q1q1 + _q2q2 - _q3q3) * By + (_2q2q3 - _2q1q0) * Bz;
+
+	// Normalize the magnetic vector, it will now be a unit vector in the horizontal plane of the NED frame
+	T reciprocal = 1.0 / sqrt(Bex * Bex + Bey * Bey);
+	Bex *= reciprocal;
+	Bey *= reciprocal;
+
+	// Transform the vector back to the body frame
+	//
+	// Bb = DCM * Be
+	Bx = (_q0q0 + _q1q1 - _q2q2 - _q3q3) * Bex + (_2q1q2 + _2q3q0) 				 * Bey;
+	By = (_2q1q2 - _2q3q0)			     * Bex + (_q0q0 - _q1q1 + _q2q2 - _q3q3) * Bey;
+	Bz = (_2q1q3 + _2q2q0)				 * Bex + (_2q2q3 - _2q1q0)				 * Bey;
+
+	// Normalize accelerometer measurement
+	reciprocal = 1.0 / std::sqrt(Bx * Bx + By * By + Bz * Bz);
+	Bx *= reciprocal;
+	By *= reciprocal;
+	Bz *= reciprocal;
+	
+	// Innovation vector 'y' being the difference between actual observation vector 'z' and
+	// recreation of observation vector 'zhat' from the current state estimate 'xhat'
+	//
+	// The following rotation matrix (DCM) transform a vector in the Earth (NED) coordinate frame into the body frame of reference
+	//
+	// [ (q0*q0 + q1*q1 - q2*q2 - q3*q3)	2*(q1*q2 + q3*q0)					2*(q1*q3 - q2*q0)		    	]
+	// [ 2*(q1*q2 - q3*q0)    				(q0*q0 - q1*q1 + q2*q2 - q3*q3) 	2*(q2*q3 + q1*q0)				]
+	// [ 2*(q1*q3 + q2*q0)    				2*(q2*q3 - q1*q0)					(q0*q0 - q1*q1 - q2*q2 + q3*q3)	]
+	//
+	// In the Earth (NED) frame two reference vectors exist, one being the acceleration vector [0 0 1]' pointing at Earth's center
+	// and the second one being the magnetic field vector [1 0 0]' pointing at Earth's magnetic north.
+	//
+	// Our task here is to recreate those vectors from the current estimated state and express them in the body frame of reference.
+	//
+	// When the rotation matrix is used for transforming the NED gravitation and magnetic vectors into the body frame, the former only receives
+	// contribution from the third column, and the latter only from the first column.
+	//
+	const T y[6] =
+	{
+		0,
+		0,
+		0,
+		Bx - (_q0q0 + _q1q1 - _q2q2 - _q3q3),
+		By - (_2q1q2 - _2q3q0),
+		Bz - (_2q1q3 + _2q2q0)
+	};
+
+	T S;		// The innovation covariance 'S_k'
+	T Sinv;		// The inverse of the innovation covariance 'S_k'
+	T K[7];		// The Kalman gain 'K_k'
+	T HP[7];	// Used for calculating both 'S_k' and 'K_k'
+
+	// Sequential update, see references: 
+    // 1) KALMAN FILTERING Theory and Practice Using MATLAB, 4.2.2, p. 141, 3:rd
+    // 2) http://www.anuncommonlab.com/articles/how-kalman-filters-work/part2.html#speed
+	for (uint8_t m=3; m<6; ++m) {
+
+		// Calculate the m:th row of the 'H_k * P_k|k-1' multiplication product
+		//
+		// Iterate the columns of 'P_k|k-1'
+		for (uint8_t i=0; i<7; ++i) {
+
+			HP[i] = H[m][0]*m_P[0][i] + H[m][1]*m_P[1][i] + H[m][2]*m_P[2][i] + H[m][3]*m_P[3][i];
+		}
+
+		// Calculate the innovation covariance 'S_k = H_k * P_k|k-1 * H'_k + R_k'
+		S = HP[0]*H[m][0] + HP[1]*H[m][1] + HP[2]*H[m][2] + HP[3]*H[m][3] + m_Ra;
+		//
+		// Multiplying is faster than dividing (so divide once)
+		Sinv = 1.0 / (S + AHRS_SAFE_EPSILON);
+
+		// Calculate the Kalman gain 'K_k = (H_k * P_k|k-1) / S
+		for (uint8_t i=0; i<7; ++i) {
+
+			K[i] = HP[i]*Sinv;
+		}
+
+		// Update the state estimate covariance 'P_k|k = P_k|k-1 - K_k * H_k * P_k|k-1'
+		for (uint8_t i=0; i<7; ++i) {
+
+			// Exploit mirrored values over the diagonal
+			for (uint8_t j=i; j<7; ++j) {
+
+				m_P[i][j] = m_P[j][i] = m_P[i][j] - K[i]*HP[j];
+			}
+		}
+
+		// Update the state estimate 'xhat_k|k = xhat_k|k-1 + K_k * y_k'
+		m_q0 += K[0] * y[m];
+		m_q1 += K[1] * y[m];
+		m_q2 += K[2] * y[m];
+		m_q3 += K[3] * y[m];
+		m_bwx += K[4] * y[m];
+		m_bwy += K[5] * y[m];
+		m_bwz += K[6] * y[m];
+	}
+}
+
+// Explicit declarations
+template class AHRS<float>;
+template class AHRS<double>;
+
+} /* namespace */
+
+//	const float DECLINATION = 5.0f*M_PI/180.0f;
+//
+//	const float cos = cosf(DECLINATION);
+//	const float sin = sinf(DECLINATION);
+//
+//	const float H[6][7] =
+//	{
+//		{ -_2q2,				_2q3,						-_2q0,						_2q1,						0.0,	0.0,	0.0 },
+//		{ _2q1,					_2q0,						_2q3,						_2q2,						0.0,	0.0,	0.0 },
+//		{ 0.0,					-2.0*_2q1,					-2.0*_2q2,					0.0,						0.0,	0.0,	0.0 },
+//		{ -_2q3*sin,			-_2q2*sin,					-2.0*_2q2*cos - _2q1*sin,	-2.0*_2q3*cos - _2q0*sin,	0.0,	0.0, 	0.0 },
+//		{ -_2q3*cos,			_2q2*cos + 2.0*_2q1*sin,	_2q1*cos,					-_2q0*cos + 2.0*_2q3*sin,	0.0,	0.0, 	0.0 },
+//		{ _2q2*cos + _2q1*sin,	_2q3*cos + _2q0*sin,		_2q0*cos - _2q3*sin,		_2q1*cos - _2q2*sin,		0.0,	0.0, 	0.0 }
+//	};
+// Magnetic declination included
+//	const float y[6] =
+//	{
+//		ax - (2.0f * (q1*q3 - q2*q0)),
+//		ay - (2.0f * (q2*q3 + q1*q0)),
+//		az - (1.0f - 2.0f * (q1*q1 + q2*q2)),
+//
+//		Bx - (1.0f - 2.0f * (q2*q2 + q3*q3))*cos - (2.0f * (q1*q2 - q3*q0))*sin,
+//		By - (2.0f * (q1*q2 - q3*q0))*cos - (1.0f - 2.0f * (q1*q1 + q2*q2))*sin,
+//		Bz - (2.0f * (q1*q3 + q2*q0))*cos - (2.0f * (q2*q3 + q1*q0))*sin
+//	};
---
+++ ahrs.h
@@ -0,0 +1,258 @@
+/*
+ *	Copyright 2020 Robin Lilja <robin.lilja@gmail.com>
+ *
+ *  Created on: Jun 26, 2015
+ *      Author: Robin Lilja
+ * 		License: MIT License (see LICENSE file)
+ */
+
+#ifndef _AHRS_EKF_H_
+#define _AHRS_EKF_H_
+
+#include <cmath>      	// std::sqrt()
+
+#define AHRS_UNROLLED	///< Makes the filter to unroll the matrix calculations
+
+namespace AHRS {
+
+static constexpr double AHRS_SAFE_EPSILON = 1e-9;
+
+/** 
+ * Attitude Heading Reference System implemented by an Extended Kalman Filter. 
+ */
+template <typename T>
+class AHRS {
+
+public:
+
+	/**
+	 * Constructor.
+	 * 
+	 * \param Q_attitude attitude quaternion process covariance
+	 * \param Q_bias gyro bias process covariance
+	 * \param R_acc accelerometer measurement covariance
+	 * \param R_mag magnetometer measurement covariance
+	 */
+	AHRS<T>(const T &Q_attitude, const T &Q_bias, const T &R_acc, const T &R_mag) {
+
+		// Make sure initial quaternion is a valid
+		m_q0 = 1.0;
+		m_q1 = m_q2 = m_q3 = 0.0;
+
+		// Assume no bias at start
+		m_bwx = m_bwy = m_bwz = 0.0;
+
+		m_Qa = Q_attitude;
+		m_Qb = Q_bias;
+
+		m_Ra = R_acc;
+		m_Rm = R_mag;
+	};
+
+	/**
+	 * Time update (propagation) of the state estimate.
+	 * \param wx x-axis angular rate [rad/s]
+	 * \param wy y-axis angular rate [rad/s] 
+	 * \param wz z-axis angular rate [rad/s] 
+	 * \param dt time delta [s]
+	 */
+	void propagate(const T &wx, const T &wy, const T &wz, const T &dt);
+
+	/**
+	 * Accelerometer measurement update (provides roll and pitch correction).
+	 * \param ax x-axis accelerometer measurement [m.s-2]
+	 * \param ay y-axis accelerometer measurement [m.s-2]
+	 * \param az z-axis accelerometer measurement [m.s-2]
+	 */
+	void accelerometer_update(T ax, T ay, T az);
+
+	/**
+	 * Magnetometer measurement update (provides yaw correction).
+	 * \param Bx x-axis magnetometer measurement [uT]
+	 * \param By y-axis magnetometer measurement [uT]
+	 * \param Bz z-axis magnetometer measurement [uT]
+	 */
+	void magnetometer_update(T Bx, T By, T Bz);
+
+	/**
+	 * Get angular rates (sensor frame).
+	 * \param rolldot roll rate [rad/s].
+	 * \param pitchdot pitch rate [rad/s].
+	 * \param yawdot yaw rate [rad/s].
+	 */
+	void getRate(T* rolldot, T* pitchdot, T* yawdot) { *rolldot = m_wx; *pitchdot = m_wy; *yawdot = m_wz; };
+
+	void getBias(T* bwx, T* bwy, T* bwz) { *bwx = m_bwx; *bwy = m_bwy; *bwz = m_bwz; };
+
+	void getAttitude(T* roll, T* pitch, T* yaw) { T q[4] = {m_q0, m_q1, m_q2, m_q3}; q2euler(q, roll, pitch, yaw); };
+
+	void getAttitude(T q[4]) { q[0] = m_q0; q[1] = m_q1; q[2] = m_q2; q[3] = m_q3; };
+
+	//void getAttitude(float dcm[3][3]) { float q[4] = {this->q0, this->q1, this->q2, this->q3}; q2dcm(q, dcm);  }
+
+	/**
+	 * Attitude state quaternion (q = w + ix + jy + kz).
+	 */
+	T m_q0, m_q1, m_q2, m_q3;
+
+	/**
+	 * True (bias compensated) angular rate (sensor frame) [rad/s].
+	 */
+	T m_wx, m_wy, m_wz;
+
+	/**
+	 * Gyro bias (sensor frame) [rad/s].
+	 */
+	T m_bwx, m_bwy, m_bwz;
+
+	/**
+	 * Process noise covariance of attitude quaternion.
+	 * This one more or less sets how much our model deviates from reality.
+	 */
+	T m_Qa;
+
+	/**
+	 * Process noise covariance of gyro bias.
+	 * Gyro bias process noise is kept near zero since we don't expect it to vary much.
+	 */
+	T m_Qb;
+
+	/**
+	 * Accelerometer measurement noise covariance.
+	 */
+	T m_Ra;
+
+	/**
+	 * Magnetometer measurement noise covariance.
+	 */
+	T m_Rm;
+
+private:
+
+#ifndef PI
+	static constexpr T PI = T(3.141592653589793);
+#endif
+
+	static constexpr T PI_HALF = T(PI / 2.0);
+
+	/**
+	 * State estimate covariance matrix 'P'.
+	 */
+	T m_P[7][7] =
+	{
+		{ 100,     0.0,      0.0,     0.0,	0.0,	0.0,	0.0 },
+		{ 0.0,     100,      0.0,     0.0,	0.0,	0.0,	0.0 },
+		{ 0.0,     0.0,      100,     0.0,	0.0,	0.0,	0.0 },
+		{ 0.0,     0.0,      0.0,     100,	0.0,	0.0,	0.0 },
+		{ 0.0,     0.0,      0.0,     0.0,	1.0,	0.0,	0.0 },
+		{ 0.0,     0.0,      0.0,     0.0,	0.0,	1.0,	0.0 },
+		{ 0.0,     0.0,      0.0,     0.0,	0.0,	0.0,	1.0 }
+	};
+
+	/**
+	 * Convert quaternion to Euler angles.
+	 * @param q quaternion (q = w + ix + jy + kz).
+	 * @param roll [rad].
+	 * @param pitch [rad].
+	 * @param yaw [rad].
+	 */
+	void q2euler(const T q[4], T* roll, T* pitch, T* yaw);
+
+	/**
+	 * Convert quaternion to DCM.
+	 * @param q quternion (q = w + ix + jy + kz).
+	 * @param dcm DCM (dcm[row][column]).
+	 */
+	void q2dcm(const T q[4], T dcm[3][3]);
+
+	/**
+	 * Convert DCM to Euler angles.
+	 * @param dcm DCM (dcm[row][column]).
+	 * @param roll [rad].
+	 * @param pitch [rad].
+	 * @param yaw [rad].
+	 */
+	void dcm2euler(const T dcm[3][3], T* roll, T* pitch, T* yaw);
+};
+
+template <typename T>
+inline void AHRS<T>::q2euler(const T q[4], T* roll, T* pitch, T* yaw) {
+
+    T dcm[3][3];
+
+    q2dcm(q, dcm);
+    dcm2euler(dcm, roll, pitch, yaw);
+}
+
+template <typename T>
+inline void AHRS<T>::q2dcm(const T q[4], T dcm[3][3]) {
+
+	// ZYX rotational order DCM
+
+    // http://www.mathworks.se/help/aeroblks/quaternionstorotationangles.html
+
+    const T q0q0 = q[0] * q[0];
+    const T q1q1 = q[1] * q[1];
+    const T q2q2 = q[2] * q[2];
+    const T q3q3 = q[3] * q[3];
+
+    const T q0q1 = q[0] * q[1];
+    const T q0q2 = q[0] * q[2];
+    const T q0q3 = q[0] * q[3];
+    const T q1q2 = q[1] * q[2];
+    const T q1q3 = q[1] * q[3];
+    const T q2q3 = q[2] * q[3];
+
+    dcm[0][0] = q0q0 + q1q1 - q2q2 - q3q3;
+    dcm[0][1] = 2.0  * (q1q2 + q0q3);
+    dcm[0][2] = 2.0  * (q1q3 - q0q2);
+
+    dcm[1][0] = 2.0  * (q1q2 - q0q3);
+    dcm[1][1] = q0q0 - q1q1 + q2q2 - q3q3;
+    dcm[1][2] = 2.0  * (q2q3 + q0q1);
+
+    dcm[2][0] = 2.0  * (q1q3 + q0q2);
+    dcm[2][1] = 2.0  * (q2q3 - q0q1);
+    dcm[2][2] = q0q0 - q1q1 - q2q2 + q3q3;
+}
+
+template <typename T>
+inline void AHRS<T>::dcm2euler(const T dcm[3][3], T* roll, T* pitch, T* yaw) {
+
+    // ZYX rotation order
+
+    // http://www.mathworks.se/help/aeroblks/quaternionstorotationangles.html
+
+    // Roll = phi
+    // Pitch = theta
+    // Yaw = psi
+
+    static constexpr T EPSILON = 1.0e-6;
+
+    *pitch = std::asin(-dcm[0][2]);
+
+    // If pitch equals +/- PI/2 we have a gimbal lock situation
+
+	// No gimbal lock
+	if ( std::abs(std::abs(*pitch) - PI_HALF) > EPSILON ) {
+
+        *roll = std::atan2(dcm[1][2], dcm[2][2]);
+        *yaw = std::atan2(dcm[0][1], dcm[0][0]);
+    }
+    // Gimbal lock (here roll and yaw give the same rotation)
+    else {
+
+        // Assume roll to be zero
+        *roll = 0.0;
+
+        // Pitch near PI/2
+        if (*pitch > 0) { *yaw = -1.0 * std::atan2(-dcm[1][2], dcm[1][1]); }
+
+        // Pitch near -PI/2
+        if (*pitch < 0) { *yaw = std::atan2(-dcm[1][2], dcm[1][1]); }
+    }
+}
+
+} /* namespace */
+
+#endif /* _AHRS_EKF_H_ */
